3ee33b2a94f404da1b4e1fc53624429ca59d1b64
Lexer11l <meshkob@rambler.ru>
2015-10-14 20:34:23 +0300

    oncreateoptionsmenu added

--- app/src/main/java/innopolis/mobile/project1/MainActivity.java
+++ app/src/main/java/innopolis/mobile/project1/MainActivity.java
@@ -2,8 +2,14 @@ package innopolis.mobile.project1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
